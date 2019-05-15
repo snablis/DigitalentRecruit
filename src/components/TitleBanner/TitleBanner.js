283a3b4ab81d0805300d0adc28aebdfedfc28be1
Mikael Sahleström <mikael.sahlestrom@scania.com>
2019-05-15 13:35:06 +0200

    created layout for titlebanner

--- src/components/TitleBanner/TitleBanner.js
+++ src/components/TitleBanner/TitleBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
+import classes from './TitleBanner.module.css';
 
+const titleBanner = (props) => (
+    <div className={classes.titleBanner}>
+        <p>
+            Text which will be seen as a banner on top of the page
+            </p>
+    </div>
+)
+
+export default titleBanner
--- src/containers/TopPage.js
+++ src/containers/TopPage.js
@@ -1,31 +1,32 @@
 import React from 'react';
+import TitleBanner from '../components/TitleBanner/TitleBanner'
 
 class TopPage extends React.Component {
     componentDidMount() {
         // Add function to fetch data from database
     }
-state = {
-    User: {
-        Name: '',
-        Age: 0,
-        Email:'',
-        Adress:'',
-        RecruitmentStep: ''
-    }}
+    state = {
+        User: {
+            Name: '',
+            Age: 0,
+            Email: '',
+            Adress: '',
+            RecruitmentStep: ''
+        }
+    }
 
-    render(){
-        return(
-// Graphic introduction on the page
+    render() {
+        return (
+            // Graphic introduction on the page
+            <TitleBanner />
 
-// Button to add new candidate
+            // Button to add new candidate.
 
-// Form to fill with new candidate information
+            // Form to fill with new candidate information. 
 
-// Searchbar && filter options
+            // Searchbar && filter options.
 
-// Container with current candidates
-
-            'Tja'
+            // Container with current candidates
         )
     }
 }
