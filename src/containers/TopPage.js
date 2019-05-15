f243b1e201aa528d888c42f55be43f3801a5d744
Mikael Sahleström <mikael.sahlestrom@scania.com>
2019-05-15 13:14:51 +0200

    fix layout

---
+++ src/containers/TopPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class TopPage extends React.Component {
+    componentDidMount() {
+        // Add function to fetch data from database
+    }
+state = {
+    User: {
+        Name: '',
+        Age: 0,
+        Email:'',
+        Adress:'',
+        RecruitmentStep: ''
+    }}
+
+    render(){
+        return(
+// Graphic introduction on the page
+
+// Button to add new candidate
+
+// Form to fill with new candidate information
+
+// Searchbar && filter options
+
+// Container with current candidates
+
+            'Tja'
+        )
+    }
+}
+
+export default TopPage
